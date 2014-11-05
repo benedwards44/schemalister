bb9b61187bdaa3a1dae52aed67ea89928fe8ad85
Ben Edwards <bedwards@tquila.com>
2014-11-05 17:50:23 +0000

    Fix integer error

--- getschema/get_schema.py
+++ getschema/get_schema.py
@@ -10,12 +10,12 @@ def get_objects_and_fields(instance_url, api_version, org_id, access_token):
 	schema.api_version = str(api_version) + '.0'
 
 	# get username of the authenticated user
-	r = requests.get(instance_url + '/services/data/v' + api_version + '.0/sobjects/User/' + user_id + '?fields=Username', headers={'Authorization': 'OAuth ' + access_token})
+	r = requests.get(instance_url + '/services/data/v' + str(api_version) + '.0/sobjects/User/' + user_id + '?fields=Username', headers={'Authorization': 'OAuth ' + access_token})
 	query_response = json.loads(r.text)
 	schema.username = query_response['Username']
 
 	# get the org name of the authenticated user
-	r = requests.get(instance_url + '/services/data/v' + api_version + '.0/sobjects/Organization/' + org_id + '?fields=Name', headers={'Authorization': 'OAuth ' + access_token})
+	r = requests.get(instance_url + '/services/data/v' + str(api_version) + '.0/sobjects/Organization/' + org_id + '?fields=Name', headers={'Authorization': 'OAuth ' + access_token})
 	schema.org_name = json.loads(r.text)['Name']
 
 	schema.save()
