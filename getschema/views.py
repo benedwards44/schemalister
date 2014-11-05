c3e59be700497ba9fa8951d7813e3fbd7fda75d5
Ben Edwards <bedwards@tquila.com>
2014-11-05 13:07:53 +0000

    Move schema logic to separate method and class

--- getschema/views.py
+++ getschema/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from getschema.models import Schema, Object, Field, Debug
 from getschema.forms import LoginForm
 from django.conf import settings
+from get_schema import get_objects_and_fields
 import json	
 import requests
 
@@ -101,110 +102,9 @@ def oauth_response(request):
 
 			if 'get_schema' in request.POST:
 
-				# create the schema record to store results
-				schema = Schema()
-				schema.org_id = org_id
-				schema.api_version = str(api_version) + '.0'
-				schema.save()
+				schema_id = get_objects_and_fields(instance_url, api_version, org_id, access_token)
 
-				# List of standard objects to include
-				standard_objects = (
-					'Account',
-					'AccountContactRole',
-					'Activity',
-					'Asset',
-					'Campaign',
-					'CampaignMember',
-					'Case',
-					'CaseContactRole',
-					'Contact',
-					'ContentVersion',
-					'Contract',
-					'ContractContactRole',
-					'Event',
-					'ForecastingAdjustment',
-					'ForecastingQuota',
-					'Lead',
-					'Opportunity',
-					'OpportunityCompetitor',
-					'OpportunityContactRole',
-					'OpportunityLineItem',
-					'Order',
-					'OrderItem',
-					'PartnerRole',
-					'Pricebook2',
-					'PricebookEntry',
-					'Product2',
-					'Quote',
-					'QuoteLineItem',
-					'Solution',
-					'Task',
-					'User',
-				)
-
-				# Describe all sObjects
-				all_objects = requests.get(
-					instance_url + '/services/data/v' + str(api_version) + '.0/sobjects/', 
-					headers={
-						'Authorization': 'Bearer ' + access_token, 
-						'content-type': 'application/json'
-					}
-				)
-
-				for sObject in all_objects.json()['sobjects']:
-
-					if sObject['name'] in standard_objects or sObject['name'].endswith('__c'):
-
-						# Create object record
-						new_object = Object()
-						new_object.schema = schema
-						new_object.api_name = sObject['name']
-						new_object.label = sObject['label']
-						new_object.save()
-
-						# query for fields in the object
-						all_fields = requests.get(instance_url + sObject['urls']['describe'], headers={'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'})
-
-						# Loop through fields
-						for field in all_fields.json()['fields']:
-
-							# Create field
-							new_field = Field()
-							new_field.object = new_object
-							new_field.api_name = field['name']
-							new_field.label = field['label']
-
-							# lookup field
-							if field['type'] == 'reference':
-								new_field.data_type = 'Lookup ('
-
-								# Could be a list of reference objects
-								for referenceObject in field['referenceTo']:
-									new_field.data_type = new_field.data_type + referenceObject.title() + ', '
-
-								# remove trailing comma and add closing bracket
-								new_field.data_type = new_field.data_type[:-2]
-								new_field.data_type = new_field.data_type + ')'
-
-							# picklist values
-							elif field['type'] == 'picklist' or field['type'] == 'multipicklist':
-								new_field.data_type = field['type'].title() + ' ('
-
-								# Add in picklist values
-								for picklist in field['picklistValues']:
-									new_field.data_type = new_field.data_type + picklist['label'] + ', '
-
-								# remove trailing comma and add closing bracket
-								new_field.data_type = new_field.data_type[:-2]
-								new_field.data_type = new_field.data_type + ')'
-
-							# everything else	
-							else:
-								new_field.data_type = field['type'].title()
-
-							new_field.save()
-
-				return HttpResponseRedirect('/schema/' + str(schema.id))
+				return HttpResponseRedirect('/schema/' + schema_id)
 
 	return render_to_response('oauth_response.html', RequestContext(request,{'error': error_exists, 'error_message': error_message, 'username': username, 'org_name': org_name, 'login_form': login_form}))
 
