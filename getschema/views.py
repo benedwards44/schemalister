9e60ada875bbaf9609e88693f41665aba45fa8bf
Ben Edwards <bedwards@tquila.com>
2014-11-05 13:39:00 +0000

    Missing import and template tidy

--- getschema/views.py
+++ getschema/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from getschema.models import Schema, Object, Field, Debug
 from getschema.forms import LoginForm
 from django.conf import settings
