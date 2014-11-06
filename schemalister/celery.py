fb69726643d872ea3d3628a47c7b519cb54474f1
Ben Edwards <bedwards@tquila.com>
2014-11-06 11:10:20 +0000

    Removed autodiscover

--- schemalister/celery.py
+++ schemalister/celery.py
@@ -9,12 +9,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schemalister.settings')
 
 app = Celery('schemalister')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
