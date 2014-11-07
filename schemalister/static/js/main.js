fc6349774adc64b528fc19074b50656642eeaf37
Ben Edwards <bedwards@tquila.com>
2014-11-07 15:37:54 +0000

    Scroll to top logic

--- schemalister/static/js/main.js
+++ schemalister/static/js/main.js
@@ -3,6 +3,23 @@ $(document).ready(function() {
 	$('.loading').hide();
 
 	$('.logging_out').hide();
+
+	/* Scroll to top logic */
+	var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
 	
 });
 
