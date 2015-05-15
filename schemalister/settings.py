a0b7749ed720cd19a13f6acf85c4ddb80c8010a8
Ben Edwards <bedwards@tquila.com>
2015-05-15 12:06:30 +0100

    Debugging

--- schemalister/settings.py
+++ schemalister/settings.py
@@ -86,3 +86,44 @@ SALESFORCE_CONSUMER_KEY = '3MVG99qusVZJwhsnmdmjq1uHdUu3eAS.rhnVu82XCV73DzcMpL0T_
 SALESFORCE_CONSUMER_SECRET = '1695084847467323776'
 SALESFORCE_REDIRECT_URI = 'https://schemalister.herokuapp.com/oauth_response'
 SALESFORCE_API_VERSION = 33
+
+# EMAIL SERVER SETTINGS
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ben@benedwards.co.nz'
+EMAIL_HOST_PASSWORD = '656y58nz'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR + '/debug.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
