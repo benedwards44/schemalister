c2b93db540389dd5175336f6db5644f76eb12695
Ben Edwards <bedwards@tquila.com>
2015-05-15 12:12:07 +0100

    Removed auto-delete on view

--- getschema/management/commands/delete_schemas.py
+++ getschema/management/commands/delete_schemas.py
@@ -6,9 +6,11 @@ class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         
+        """
         one_hour_ago = datetime.datetime.now() - datetime.timedelta(minutes=60)
         schemas = Schema.objects.filter(finished_date__lt = one_hour_ago)
         schemas.delete()
+        """
 
         one_day_ago = datetime.datetime.now() - datetime.timedelta(hours=24)
         schemas = Schema.objects.filter(created_date__lt = one_day_ago)
