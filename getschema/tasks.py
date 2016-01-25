e36ed3d896c52363c5856629b94acf57cba3362a
System Administrator <root@Bens-MacBook-Pro.local>
2016-01-25 14:02:37 +0000

    Updated to use metadata API

--- getschema/tasks.py
+++ getschema/tasks.py
@@ -13,6 +13,7 @@ from getschema.models import Schema, Object, Field, Debug
 from django.conf import settings
 import json	
 import requests
+from suds.cient import Client
 
 @app.task
 def get_objects_and_fields(schema): 
@@ -70,6 +71,26 @@ def get_objects_and_fields(schema):
 
 		if 'sobjects' in all_objects.json():
 
+			# Moved logic to metadata API to get field names for fields
+			metadata_client = Client('https://schemalister.herokuapp.com/static/metadata-35.wsdl.xml')
+			
+			# URL for metadata API
+			metadata_url = instance_url + '/services/Soap/m/' + str(settings.SALESFORCE_API_VERSION) + '.0/' + org_id
+			
+			# Set location for the client
+			metadata_client.set_options(location = metadata_url)
+
+			# Set the header for the client
+			session_header = metadata_client.factory.create("SessionHeader")
+			session_header.sessionId = access_token
+			metadata_client.set_options(soapheaders = session_header)
+
+			# List of names of the objects to query
+			object_list = []
+
+			# Count how many objects left
+			loop_counter = 0;
+
 			for sObject in all_objects.json()['sobjects']:
 
 				if sObject['name'] in standard_objects or sObject['name'].endswith('__c'):
@@ -81,78 +102,99 @@ def get_objects_and_fields(schema):
 					new_object.label = sObject['label']
 					new_object.save()
 
-					# query for fields in the object
-					all_fields = requests.get(instance_url + sObject['urls']['describe'], headers={'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'})
+					# Append the object to the list to query
+					object_list.append(sObject['name'])
 
-					# Loop through fields
-					for field in all_fields.json()['fields']:
+					# Run the metadata query only if the list has reached 10 (the max allowed to query)
+					# at one time, or if there is less than 10 components left to query 
+					if len(object_list) >= 10 or (len(all_objects.json()['sobjects']) - loop_counter) <= 10:
 
-						# Create field
-						new_field = Field()
-						new_field.object = new_object
-						new_field.api_name = field['name']
-						new_field.label = field['label']
+						# Query for the sobjects
+						sobjects_result = metadata_client.service.readMetadata('CustomObject', object_list)
 
-						if 'inlineHelpText' in field:
-							new_field.help_text = field['inlineHelpText']
+						# Query for the objects
+						for sobject in sobjects_result[0]:
 
-						# If a formula field, set to formula and add the return type in brackets
-						if 'calculated' in field and (field['calculated'] == True or field['calculated'] == 'true'):
-							new_field.data_type = 'Formula (' + field['type'] + ')'
+							if 'fields'in sobject:
 
-						# lookup field
-						elif field['type'] == 'reference':
+								# Loop through fields
+								for field in sobject.fields:
 
-							new_field.data_type = 'Lookup ('
+									# Create field
+									new_field = Field()
+									new_field.object = new_object
+									new_field.api_name = field.fullName
+									new_field.label = fullName.label
 
-							# Could be a list of reference objects
-							for referenceObject in field['referenceTo']:
-								new_field.data_type = new_field.data_type + referenceObject.title() + ', '
+									if 'description'in field:
+										new_field.description = field['description']
 
-							# remove trailing comma and add closing bracket
-							new_field.data_type = new_field.data_type[:-2]
-							new_field.data_type = new_field.data_type + ')'
+									if 'inlineHelpText' in field:
+										new_field.help_text = field['inlineHelpText']
 
-						# picklist values
-						elif field['type'] == 'picklist' or field['type'] == 'multipicklist':
+									# If a formula field, set to formula and add the return type in brackets
+									if 'calculated' in field and (field['calculated'] == True or field['calculated'] == 'true'):
+										new_field.data_type = 'Formula (' + field['type'] + ')'
 
-							new_field.data_type = field['type'].title() + ' ('
+									# lookup field
+									elif field['type'] == 'reference':
 
-							# Add in picklist values
-							for picklist in field['picklistValues']:
-								new_field.data_type = new_field.data_type + picklist['label'] + ', '
+										new_field.data_type = 'Lookup ('
 
-							# remove trailing comma and add closing bracket
-							new_field.data_type = new_field.data_type[:-2]
-							new_field.data_type = new_field.data_type + ')'
+										# Could be a list of reference objects
+										for referenceObject in field['referenceTo']:
+											new_field.data_type = new_field.data_type + referenceObject.title() + ', '
 
-						# if text field, add field length
-						elif field['type'] == 'string' or field['type'] == 'textarea':
+										# remove trailing comma and add closing bracket
+										new_field.data_type = new_field.data_type[:-2]
+										new_field.data_type = new_field.data_type + ')'
 
-							new_field.data_type = field['type'].title()
+									# picklist values
+									elif field['type'] == 'picklist' or field['type'] == 'multipicklist':
 
-							# Add the field length to the title
-							if 'length' in field:
-								new_field.data_type += ' (' + str(field['length']) + ')'
+										new_field.data_type = field['type'].title() + ' ('
 
-						# If number, currency or percent
-						elif field['type'] == 'double' or field['type'] == 'percent' or field['type'] == 'currency':
+										# Add in picklist values
+										for picklist in field.picklist['picklistValues']:
+											new_field.data_type = new_field.data_type + picklist['label'] + ', '
 
-							new_field.data_type = field['type'].title()
+										# remove trailing comma and add closing bracket
+										new_field.data_type = new_field.data_type[:-2]
+										new_field.data_type = new_field.data_type + ')'
 
-							# Add the length and precision
-							if 'precision' in field and 'scale' in field:
+									# if text field, add field length
+									elif field['type'] == 'string' or field['type'] == 'textarea':
 
-								# Determine the length
-								length = int(field['precision']) - int(field['scale'])
+										new_field.data_type = field['type'].title()
 
-								# Add length and scale to the field type
-								new_field.data_type += ' (' + str(length) + ',' + str(field['scale']) + ')'
+										# Add the field length to the title
+										if 'length' in field:
+											new_field.data_type += ' (' + str(field['length']) + ')'
 
-						else:
-							new_field.data_type = field['type'].title()
+									# If number, currency or percent
+									elif field['type'] == 'double' or field['type'] == 'percent' or field['type'] == 'currency':
 
-						new_field.save()
+										new_field.data_type = field['type'].title()
+
+										# Add the length and precision
+										if 'precision' in field and 'scale' in field:
+
+											# Determine the length
+											length = int(field['precision']) - int(field['scale'])
+
+											# Add length and scale to the field type
+											new_field.data_type += ' (' + str(length) + ',' + str(field['scale']) + ')'
+
+									else:
+										new_field.data_type = field['type'].title()
+
+									new_field.save()
+
+						# Clear the object list now
+						object_list = []
+
+					# Increment the count
+					loop_counter = loop_counter + 1
 
 			schema.status = 'Finished'
 
