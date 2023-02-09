b696592f5c293ce27bc6d9834fd91a3c288a5af5
Ben Edwards <ben@edwards.nz>
2023-02-09 19:33:57 +1300

    Remove logging

--- schemalister/settings.py
+++ schemalister/settings.py
@@ -112,38 +112,3 @@ SALESFORCE_REDIRECT_URI = 'https://schemalister.herokuapp.com/oauth_response'
 SALESFORCE_API_VERSION = os.environ.get('SALESFORCE_API_VERSION', '55')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-if IS_HEROKU:
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
-            }
-        },
-        'handlers': {
-            'mail_admins': {
-                'level': 'ERROR',
-                'filters': ['require_debug_false'],
-                'class': 'django.utils.log.AdminEmailHandler'
-            },
-            'file': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': BASE_DIR / '/debug.log',
-            },
-        },
-        'loggers': {
-            'django.request': {
-                'handlers': ['mail_admins', 'file'],
-                'level': 'ERROR',
-                'propagate': True,
-            },
-        }
-    }
